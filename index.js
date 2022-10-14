25e3361b6177e7e5f12245e4fc4a53abf158a6bf
raul victor <raulvictormello@hotmail.com>
2022-10-13 21:53:08 -0300

    jogo funcionando

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 
 const mario= document.querySelector('.mario');
+const barril= document.querySelector('.barril');
 
 const pulo=()=>{
    mario.classList.add('pulo')      
+   
   
    setTimeout(()=>{
     mario.classList.remove('pulo')
-
-
    },600)
+  
    
  }
  
+const loop = setInterval(()=>{
+const barrilfim = barril.offsetLeft;
+const mariopula = +window.getComputedStyle(mario).bottom.replace("px", "")
+
+if(barrilfim<=96  &&barrilfim>0 && mariopula<=80){     
+  barril.style.animation ="none" 
+  barril.style.left =`${ barrilfim}px`;
+ 
+  mario.style.animation ="none" 
+  mario.style.bottom=`${mariopula}px`;
+  mario.src ='./imagens/game-over.png'
+  mario.style.width ="65px"
+  mario.style.marginleft ="100px"
+  mario.style.height='90px';
+
+  clearInterval(loop)
+  
+ }
+  
+},100)
+
+console.log ()
+
+
 document.addEventListener('keydown', pulo)
