5283a0642110c128377aba1a581082e501879b45
raul victor <raulvictormello@hotmail.com>
2022-10-14 20:57:51 -0300

    adicionando contador

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 
 const mario= document.querySelector('.mario');
 const barril= document.querySelector('.barril');
-
+const contador = document.getElementById('contador');
+const moverNuvem = document.getElementById('.moverNuvem');
+tempo =1;
 const pulo=()=>{
    mario.classList.add('pulo')      
    
   
    setTimeout(()=>{
-    mario.classList.remove('pulo')
+    mario.classList.remove('pulo')   
    },600)
-  
-   
  }
- 
+
+const contaa =setInterval(()=>{
+   
+  contador.innerHTML =`Tempo:${tempo++}` ; 
+},1000)
+
+
 const loop = setInterval(()=>{
 const barrilfim = barril.offsetLeft;
 const mariopula = +window.getComputedStyle(mario).bottom.replace("px", "")
@@ -23,18 +29,24 @@ if(barrilfim<=96  &&barrilfim>0 && mariopula<=80){
  
   mario.style.animation ="none" 
   mario.style.bottom=`${mariopula}px`;
+  
+
+
   mario.src ='./imagens/game-over.png'
   mario.style.width ="65px"
   mario.style.marginleft ="100px"
   mario.style.height='90px';
 
   clearInterval(loop)
-  
+  clearInterval(contaa)
+ 
  }
   
 },100)
 
-console.log ()
-
-
 document.addEventListener('keydown', pulo)
+
+
+function recarregarAPagina(){
+    window.location.reload();
+} 
