1d3778521b84c7e02a06e385cc81733641cf3185
raul victor <raulvictormello@hotmail.com>
2022-10-19 20:35:30 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const mario= document.querySelector('.mario');
 const barril= document.querySelector('.barril');
+const barril2= document.querySelector('.barril2');
 const contador = document.getElementById('contador');
 const moverNuvem = document.getElementById('.moverNuvem');
 tempo =1;
@@ -16,22 +17,37 @@ const pulo=()=>{
 const contaa =setInterval(()=>{
    
   contador.innerHTML =`Tempo:${tempo++}` ; 
+
+  
 },1000)
 
 
-const loop = setInterval(()=>{
-const barrilfim = barril.offsetLeft;
-const mariopula = +window.getComputedStyle(mario).bottom.replace("px", "")
+const Barrillll= setTimeout (()=>{
+barril2.style.display= "block "
+   
+},3000)
 
-if(barrilfim<=96  &&barrilfim>0 && mariopula<=80){     
+
+const loop = setInterval(()=>{
+const barrilfim = barril.offsetLeft; 
+const maisBarrill = barril2.offsetLeft;
+
+const mariopula = +window.getComputedStyle(mario).bottom.replace("px", "")
+ 
+if( barrilfim<=96 && barrilfim>0 && mariopula<=80 ){     
   barril.style.animation ="none" 
-  barril.style.left =`${ barrilfim}px`;
+  barril.style.left =`${ barrilfim}px`; 
+   
+  barril2.style.animation ="none" 
+  barril2.style.left =`${maisBarrill}px`;
+ 
  
   mario.style.animation ="none" 
-  mario.style.bottom=`${mariopula}px`;
+  mario.style.bottom=`${mariopula}px`; 
+
+
+
   
-
-
   mario.src ='./imagens/game-over.png'
   mario.style.width ="65px"
   mario.style.marginleft ="100px"
